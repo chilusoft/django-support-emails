5792db3f548fdc526e8a1ca09bb6ce0241d6c480
chilusoft <mukukachilu@gmail.com>
2023-08-01 19:30:53 +0200

    Added routers

---
+++ index/urls.py
@@ -0,0 +1,29 @@
+# Copyright 2023 Chilufya Mukuka <mukukachilu@gmail.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rest_framework.routers import DefaultRouter
+from .views import SupportEmailView, SupportEmailResponseView, send_support_email, send_support_email_response
+from django.urls import path, include
+
+
+router = DefaultRouter()
+
+router.register('support-email/', SupportEmailView)
+router.register('support-email-response/', SupportEmailResponseView)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('send-support-email/', send_support_email),
+    path('send-support-email-response/', send_support_email_response)
+]
