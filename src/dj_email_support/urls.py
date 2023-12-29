1978568a1eb0b81952daba07011bc317d4b110e9
chilusoft <mukukachilu@gmail.com>
2023-12-29 10:54:49 +0200

    changed file structure for publication ready format

--- src/dj_email_support/urls.py
+++ src/dj_email_support/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj-support-email/api/v1/', include('index.urls'))
+    path('drf-support-email/', include('index.urls'))
 ]
